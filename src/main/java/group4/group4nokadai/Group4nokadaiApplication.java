48bb2aa336bce0320e0253a026fe627c7ffaacdb
B20037-Kobayashi <e1b20037@oit.ac.jp>
2022-10-04 14:16:16 +0900

    初期化

---
+++ src/main/java/group4/group4nokadai/Group4nokadaiApplication.java
@@ -0,0 +1,13 @@
+package group4.group4nokadai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Group4nokadaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Group4nokadaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/group4/group4nokadai/Group4nokadaiApplicationTests.java
@@ -0,0 +1,13 @@
+package group4.group4nokadai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Group4nokadaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
